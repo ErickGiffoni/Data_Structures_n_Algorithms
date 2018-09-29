d2d88a25b92c75480e9a1730b2d8f26f8bf18c56
ErickGiffoni <giffoni.erick@gmail.com>
2018-09-28 22:24:18 -0300

    Corrigido. Adicao da biblioteca ILBP_library.h e alocacao das matrizes matriz_grama_txt e vetores_ILBP_grama -> inicio do calculo da vizinhanca de 8

--- projeto2/teste/abre_arq_teste_pequeno.c
+++ projeto2/teste/abre_arq_teste_pequeno.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 //#include <string.h>
+#include "../libraries/ILBP_library.h"
 
 #define numero_de_imagens 4 //numero de imagens a serem lidas
 
@@ -13,6 +14,8 @@ FILE *file_para_grama;
 FILE *file_para_asfalto; //ponteiros para os arquivos
 
 int  vec_numeros[4][2] = { {0,1}, {0,2}, {0,3}, {0,4} };
+
+int ** vetores_ILBP_grama; //servira para armazenar o vetor ILBP de cada .txt
 
 //printf("%d %d\n\n", vec_numeros[contador+2][0], vec_numeros[contador+2][1]);
 
@@ -47,48 +50,67 @@ while(contador <numero_de_imagens){ //DataSet grama
     // CALCULAR O ILBP
 
       //encontrar o tamanho maximo da imagem .txt -> 1024 x 1024
-    fscanf(file_para_grama, "%d%c", &numero_no_arquivo, &ponto_e_virgula);
       while(!feof(file_para_grama)){
+        fscanf(file_para_grama, "%d%c", &numero_no_arquivo, &ponto_e_virgula);
 
         if(ponto_e_virgula == ';'){
           numero_de_colunas_grama ++;
         }
         else if(ponto_e_virgula == '\n'){
           numero_de_linhas_grama ++;
-          numero_de_colunas_grama --; // para desconsiderar as quebras de linha
+          numero_de_colunas_grama ++;
         }
 
-        fscanf(file_para_grama, "%d%c", &numero_no_arquivo, &ponto_e_virgula);//leia os proximos
+        //fscanf(file_para_grama, "%d%c", &numero_no_arquivo, &ponto_e_virgula);//leia os proximos
 
       }//end of while feof grama
 
-      numero_de_colunas_grama += 1; //o ultimo numero do arquivo nao e seguido de ';'
+      //numero_de_colunas_grama += 1; //o ultimo numero do arquivo nao e seguido de ';'
       numero_de_linhas_grama -= 1; //a ultima quebra de linha do arquivo nao representa uma nova linha com elementos
+      numero_de_colunas_grama /= numero_de_linhas_grama;
       printf("Tamaho de colunas : %d\nNumero de linhas : %d\n\n", numero_de_colunas_grama, numero_de_linhas_grama);
-      printf("Numero no arquivo : %d\nPonto e virgula : %c\n\n", numero_no_arquivo, ponto_e_virgula);
+      printf("Ultimo Numero no arquivo : %d\nPonto e virgula : %c\n\n", numero_no_arquivo, ponto_e_virgula);
 
       //alocar dinamicamente uma matriz para armazenar os numeros do arquivo .txt e ler esses numeros
 
         int ** matriz_grama_txt = (int **) calloc(numero_de_linhas_grama, sizeof(int *)); //alocacao de uma matriz para o arquivo txt
         for(i_for =0; i_for<numero_de_linhas_grama; i_for++){
-          matriz_grama_txt[i_for] = (int *) calloc(numero_de_linhas_grama, sizeof(int)); //alocacao das colunas da matriz com mesmo numero de linhas
-        }
+          matriz_grama_txt[i_for] = (int *) calloc(numero_de_colunas_grama, sizeof(int)); //alocacao das colunas da matriz com mesmo numero de linhas
+        } //end for aloca ,matriz_grama_txt                                              // matriz quadrada
+
+        if(matriz_grama_txt == NULL) printf("matriz_grama_txt NULL\n\n");
 
       //ler o .txt novamente e preencher a matriz_grama_txt
         rewind(file_para_grama); //o ponteiro deve voltar ao inicio do arquivo
 
         for(i_for = 0; i_for < numero_de_linhas_grama; i_for++){ //acesso as linhas
-          for(j_for =0; j_for< numero_de_linhas_grama; j_for++){ //acesso as colunas
+          for(j_for =0; j_for< numero_de_colunas_grama; j_for++){ //acesso as colunas
             if(feof(file_para_grama)){
               printf("END OF FILE %d\n", contador+1); //se o ponteiro chegar ao EOF
               break;
             }
             else fscanf(file_para_grama, "%d%c", &matriz_grama_txt[i_for][j_for], &ponto_e_virgula); //le o numero e o caractere
-          }
-printf("Elemento [][ultimo] %d\n", matriz_grama_txt[i_for][numero_de_linhas_grama-1]);
+          }                                                                                         //armazena o numero na matriz
+//printf("Elemento [][ultimo] %d\n", matriz_grama_txt[i_for][j_for-1]);
         }//end for preenche matriz
 
-printf("FREE NIGGA %d\n", contador+1);
+      //alocar dinamicamente o vetores_ILBP_grama
+
+        vetores_ILBP_grama = (int **) calloc(50, sizeof(int *));
+        for(i_for=0; i_for<50; i_for++){
+          vetores_ILBP_grama[i_for] = (int *) calloc(numero_de_linhas_grama*numero_de_colunas_grama, sizeof(int));
+        }//end for aloca vetores_ILBP_grama
+
+      //calcular a vizinhanca de 8 de cada pixel, armazenar no vetores_ILBP_grama correspondente
+        rewind(file_para_grama);
+
+        for(i_for =0; i_for<numero_de_linhas_grama; i_for++){
+          for(j_for =0; j_for<numero_de_colunas_grama; j_for++){
+
+          }
+        }//end for ILBP
+
+printf("i for : %d\nj for : %d\nFREE NIGGA %d\n", i_for, j_for, contador+1);
       free(matriz_grama_txt); //liberar o espaco em memoria
 
     //CALCULAR O GLCM
@@ -101,7 +123,7 @@ printf("FREE NIGGA %d\n", contador+1);
     numero_de_linhas_grama =0; //atualizar tambem
 
     fclose(file_para_grama);
-    printf("Fechei o %d\n", contador+1);
+    printf("Fechei o %d\n\n", contador+1);
   }
   else printf("Erro ao abrir o arquivo %d\n", contador+1);
 
@@ -145,7 +167,8 @@ while(contador <numero_de_imagens){ //DataSet asfalto
 }//end while abre asfalto
 
 printf("****************************************************************\n\n");
-
+  // FIM DO PROGRAMA
+free(vetores_ILBP_grama);
 
   return 0;
 }
