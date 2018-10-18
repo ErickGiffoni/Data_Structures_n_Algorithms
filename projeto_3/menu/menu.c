7b4dcee31bc0eb00411ecb139387a78ee462469e
ErickGiffoni <giffoni.erick@gmail.com>
2018-10-18 19:46:23 -0300

    Menu final implementado

--- projeto_3/menu/menu.c
+++ projeto_3/menu/menu.c
@@ -19,31 +19,39 @@ int main(){
 
   int opcao =0;
 
-inicio:
+  int boolean = 0;
+
+do{
   menu(&opcao); //parametro deve ser ponteiro -> passamos o endereco, entao.
 
   switch (opcao) {
     case 1:
       printf("Opcao 1\n");
+      boolean = 1;
       break;
     case 2:
       printf("Opcao 2\n");
+      boolean = 1;
       break;
     case 3:
       printf("Opcao 3\n");
+      boolean = 1;
       break;
     case 4:
       printf("Opcao 4\n");
+      boolean = 1;
       break;
     case 5:
       printf("Opcao 5\n"); // implementar a saida do programa
+      boolean = 1;
       return SAIR;
     default:
       fflush(stdin);
       opcao = 0; // erro : laco infinito se o usuario digitar algo nao numerico (letra por exemplo)
       printf("Opcao invalida, tente novamente.\n\n\n\n");
-      goto inicio;
   }
+
+}while(!boolean);
 
   return 0;
 }
