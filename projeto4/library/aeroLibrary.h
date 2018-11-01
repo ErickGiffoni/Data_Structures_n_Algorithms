f6e263a47773a9e3afe208da92abcfb94934c6ad
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-01 15:12:52 -0300

    Codigo de voo da stuct agora e vetor, e codigos de voo agora tem 7 colunas - usar strcpy

--- projeto4/library/aeroLibrary.h
+++ projeto4/library/aeroLibrary.h
@@ -5,7 +5,7 @@
 
 // ******* Estruturas *******
 typedef struct voo{
-  char **codigo_de_voo ; // ponteiro para um vetor de char, que e o codigo de voo alocado estaticamente na main
+  char codigo_de_voo[7] ; // vetor de char, que e o codigo de voo alocado estaticamente na main
   char tipo; // D -> decolagem ; A -> aproximacao ; P -> pouso
   int combustivel; // se for do tipo aproximacao, tera um numero de combustivel : 0 e pouco e 12 e muito
   struct voo *proximo ; // proximo da fila de voos
@@ -17,7 +17,7 @@ typedef struct fila{
 }Fila;
 
 // ******* Variaveis *******
-char codigos_de_voos[64][6] = {{"VG3001"}, {"JJ4404"}, {"LN7001"}, {"TG1501"}, {"GL7602"}, {"TT1010"}, {"AZ1009"}, {"AZ1008"},
+char codigos_de_voos[64][7] = {{"VG3001"}, {"JJ4404"}, {"LN7001"}, {"TG1501"}, {"GL7602"}, {"TT1010"}, {"AZ1009"}, {"AZ1008"},
                                {"AZ1010"}, {"TG1506"}, {"VG3002"}, {"JJ4402"}, {"GL7603"}, {"RL7880"}, {"AL0012"}, {"TT4544"},
                                {"TG1505"}, {"VG3003"}, {"JJ4403"}, {"JJ4401"}, {"LN7002"}, {"AZ1002"}, {"AZ1007"}, {"GL7604"},
                                {"AZ1006"}, {"TG1503"}, {"AZ1003"}, {"JJ4403"}, {"AZ1001"}, {"LN7003"}, {"AZ1004"}, {"TG1504"},
