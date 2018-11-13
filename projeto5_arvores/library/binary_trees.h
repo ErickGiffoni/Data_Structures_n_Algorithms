06e02762816c563aa2942535b8cc1e5e8adff764
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-13 18:27:34 -0200

    Estruturas Node e Tree implementadas

--- projeto5_arvores/library/binary_trees.h
+++ projeto5_arvores/library/binary_trees.h
@@ -1,6 +1,17 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+typedef struct node{
+  int value; //informacao
+  struct node *right = NULL; //ponteiro para no da direita
+  struct node *left = NULL; //ponteiro para no da esquerda
+}Node;
+
+typedef struct tree{
+  Node *root; //ponteiro para o no raiz
+  int height; //altura
+}Tree;
+
 loadTreeFromFile();
 showTree();
 isFull();
