82e7cd3d4cec918470aebf89d342d472bead7fdc
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-13 17:46:26 -0200

    Inicio da biblioteca de arvores

---
+++ projeto5_arvores/library/binary_trees.h
@@ -0,0 +1,15 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+loadTreeFromFile();
+showTree();
+isFull();
+searchValue();
+getHeight();
+removeValue();
+printInOrder();
+printPreOrder();
+printPosOrder();
+balanceTree();
+
+#endif
