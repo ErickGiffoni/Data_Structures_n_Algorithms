0b5e01a483430690ee4d690071fb8fe6233fead6
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-13 17:47:01 -0200

    Inicio da implementacao da biblioteca de arvores

---
+++ projeto5_arvores/source/binary_trees.c
@@ -0,0 +1,41 @@
+#include "../library/binary_trees.h"
+
+loadTreeFromFile(){
+
+}//end of loadTreeFromFile
+
+showTree(){
+
+}//end of showTree
+
+isFull(){
+
+}//end of isFull
+
+searchValue(){
+
+}//end of searchValue
+
+getHeight(){
+
+}//end of getHeight
+
+removeValue(){
+
+}//end of removeValue
+
+printInOrder(){
+
+}//end of printInOrder
+
+printPreOrder(){
+
+}//end of printPreOrder
+
+printPosOrder(){
+
+}//end of printPosOrder
+
+balanceTree(){
+
+}//end of balanceTree
