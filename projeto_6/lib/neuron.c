e8d0f773ad139b3568035384ea2528f9e1d0611e
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-25 16:40:31 -0200

    perf - changing neuron struct and functions to work with arrays, instead of matrices

--- projeto_6/lib/neuron.c
+++ projeto_6/lib/neuron.c
@@ -1,13 +1,18 @@
 #include "neuron.h"
+#include <math.h>
 
-double exit_neuron(Neuron *n, double **p){//funcao para fazer todo o calculo dento do neuronio, chamando as outras abaixo - recebe o neuronio, a matriz de entrada, retorna a saida s
+double exit_neuron(Neuron *n, double *p){//funcao para fazer todo o calculo dento do neuronio, chamando as outras abaixo - recebe o neuronio, o vetor de entrada, retorna a saida s
   return 0;
 }//end of exit_neuron
 
 double logistic_function(double *somatorio){
-  return 0;
+  return 1.0/(1.0 + exp(-(*somatorio)));
 }//end of logistic_function
 
-double dot_product(double **p, double **w){//funcao do produto interno - recebe a matriz de entrada e a de pesos, retorna o somatorio dos dot products
+double dot_product(double *p, double *w){//funcao do produto interno - recebe o vetor de entrada e o de pesos, retorna o somatorio dos dot products
+  double somatorio;
+  for(int i=0; i<537; i++){//dot product
+
+  }//end of for dot product
   return 0;
 }//end of dot_product
--- projeto_6/lib/neuron.h
+++ projeto_6/lib/neuron.h
@@ -2,14 +2,14 @@
 #define NEURON_H
 
 typedef struct neuron{
-  double **w; //matriz de pesos
+  double *w; //vetor de pesos
   double somatorio; //somatorio do produto interno(dot product): entrada * w
   double b; //escalar real
   double s; //saida do neuronio, apos aplicada a funcao logistica
 } Neuron;//struct neuron
 
-double exit_neuron(Neuron *, double **);//funcao para fazer todo o calculo dento do neuronio, chamando as outras abaixo - recebe o neuronio, a matriz de entrada, retorna a saida s
+double exit_neuron(Neuron *, double *);//funcao para fazer todo o calculo dento do neuronio, chamando as outras abaixo - recebe o neuronio, o vetor de entrada, retorna a saida s
 double logistic_function(double *);//recebe o resultado do dot_product + escalar b, retorna a saida s
-double dot_product(double **, double **);//funcao do produto interno - recebe a matriz de entrada e a de pesos, retorna o somatorio dos dot products
+double dot_product(double *, double *);//funcao do produto interno - recebe o vetor de entrada e o de pesos, retorna o somatorio dos dot products
 
 #endif
