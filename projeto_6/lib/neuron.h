6772a6d1175065b3306c8b62159af17d3e52a4a8
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-25 19:37:58 -0200

    perf - newNeuron function, creates calloc neuron

--- projeto_6/lib/neuron.h
+++ projeto_6/lib/neuron.h
@@ -8,6 +8,7 @@ typedef struct neuron{
   double s; //saida do neuronio, apos aplicada a funcao logistica
 } Neuron;//struct neuron
 
+Neuron *new_neuron();//criar neuronio vazio
 double exit_neuron(Neuron *, double *);//funcao para fazer todo o calculo dento do neuronio, chamando as outras abaixo - recebe o neuronio, o vetor de entrada, retorna a saida s
 double logistic_function(double *);//recebe o resultado do dot_product + escalar b, retorna a saida s
 double dot_product(double *, double *);//funcao do produto interno - recebe o vetor de entrada e o de pesos, retorna o somatorio dos dot products
