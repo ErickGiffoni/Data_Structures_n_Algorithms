fed9783a9855200c6d167c10dc217f18a2a5438b
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-25 13:52:27 -0200

    perf - struct neuron and functions created

---
+++ projeto_6/lib/neuron.c
@@ -0,0 +1,13 @@
+#include "neuron.h"
+
+double exit_neuron(Neuron *n, double **p){//funcao para fazer todo o calculo dento do neuronio, chamando as outras abaixo - recebe o neuronio, a matriz de entrada, retorna a saida s
+  return 0;
+}//end of exit_neuron
+
+double logistic_function(double *somatorio){
+  return 0;
+}//end of logistic_function
+
+double dot_product(double **p, double **w){//funcao do produto interno - recebe a matriz de entrada e a de pesos, retorna o somatorio dos dot products
+  return 0;
+}//end of dot_product
