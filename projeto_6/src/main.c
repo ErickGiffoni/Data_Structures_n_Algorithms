65fbf53c51d0dc476089aa1d6c3ba6406c658176
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-25 19:39:40 -0200

    test - neural.c working

--- projeto_6/src/main.c
+++ projeto_6/src/main.c
@@ -10,12 +10,16 @@ int main(int argc, char **argv){
     printf("numero de neuronios na camada oculta = %d\n", number_of_neurons_hidden_layer);
 
     //testing dot_product and logistic_function
-    double array1[537] = {20, 20};
+    double array1[537] = {20, 20,2,2,3,4,5,6,7};
     double array2[537] = {10,10};
     double somatorio = dot_product(array1, array2);//400
     printf("somatorio = %.5lf\n", somatorio);//400
     double saida = logistic_function(&somatorio);//1
     printf("saida = %.5lf\n", saida);//1
+    //testing neuron.h
+    Neuron *neuron = new_neuron();
+    //neuron->w = [23,12,13,14,45,6,7,32,45]; // nao da certo
+    neuron->s = exit_neuron(neuron, array1);
     //-----------------------------------------------------//
     return 0;
 }
