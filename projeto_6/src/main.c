12cc9cb9f2386e108828bc1231023f886e8fdf81
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-25 13:07:53 -0200

    feature - numero de neuronios na camada oculta via atoi()

--- projeto_6/src/main.c
+++ projeto_6/src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main(int argc, char **argv){
     printf("Hello World!\n");
+    int number_of_neurons_hidden_layer = atoi(argv[1]);//numero de neuronios na camada oculta
+    printf("numero de neuronios na camada oculta = %d\n", number_of_neurons_hidden_layer);
     return 0;
 }
