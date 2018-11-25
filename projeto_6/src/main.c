b9e667df58ab2736d812582dca069671a21aeb40
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-25 17:05:05 -0200

    test - neuron.c working

--- projeto_6/src/main.c
+++ projeto_6/src/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../lib/neuron.h"
+
+//compile: gcc main.c ../lib../neuron.c -o main
 
 int main(int argc, char **argv){
     printf("Hello World!\n");
     int number_of_neurons_hidden_layer = atoi(argv[1]);//numero de neuronios na camada oculta
     printf("numero de neuronios na camada oculta = %d\n", number_of_neurons_hidden_layer);
+
+    //testing dot_product and logistic_function
+    double array1[537] = {20, 20};
+    double array2[537] = {10,10};
+    double somatorio = dot_product(array1, array2);//400
+    printf("somatorio = %.5lf\n", somatorio);//400
+    double saida = logistic_function(&somatorio);//1
+    printf("saida = %.5lf\n", saida);//1
+    //-----------------------------------------------------//
     return 0;
 }
