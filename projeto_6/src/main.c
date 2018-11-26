7a02db432efa06ec90b7e5f6edf8717328d46e18
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-26 20:13:07 -0200

    feat - alocando as camadas de entrada, oculta e de saida em arrays

--- projeto_6/src/main.c
+++ projeto_6/src/main.c
@@ -4,6 +4,8 @@
 
 //compile: gcc main.c ../lib../neuron.c -o main
 
+#define qtd_neurons 536
+
 int main(int argc, char **argv){
     printf("Hello World!\n");
     int number_of_neurons_hidden_layer = atoi(argv[1]);//numero de neuronios na camada oculta
@@ -20,6 +22,16 @@ int main(int argc, char **argv){
     Neuron *neuron = new_neuron();
     //neuron->w = [23,12,13,14,45,6,7,32,45]; // nao da certo
     neuron->s = exit_neuron(neuron, array1);
+    //criando os arrays de camadas de neuronios
+    Neuron **first_layer = (Neuron **)calloc(qtd_neurons,sizeof(Neuron*));//array de neuronios com qtd_neurons posicoes
+    Neuron **hidden_layer = (Neuron **)calloc(number_of_neurons_hidden_layer,sizeof(Neuron *));//array de neuronios com number_of_neurons_hidden_layer posicoes
+    Neuron *last_layer = new_neuron();//ultima camada e um unico neuronio
+    for(int i=0; i<qtd_neurons; i++){
+      first_layer[i] = new_neuron();
+    }//alocaao de neuronios na primeira camada
+    for(int i=0; i<number_of_neurons_hidden_layer; i++){
+      hidden_layer[i] = new_neuron();
+    }//alocaao de neuronios camada oculta
     //-----------------------------------------------------//
     return 0;
 }
